eab36d0206492eec98da0b701868aab91e21a6a1
JULIO CESAR GOMES <julio.cgomes8@senacsp.edu.br>
2024-11-25 21:57:13 -0300

    bancoMaisConsole

---
+++ repos/CadastroCliente/CadastroCliente/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroCliente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string op;
+
+            do
+            {
+                Console.WriteLine("Seja Bem Vindos \n");
+                Console.WriteLine("Escolha uma Opção \n");
+                Console.WriteLine("1.Incluir Novo Cliente");
+                Console.WriteLine("2.Alterar Dados Do Cliente");
+                Console.WriteLine("3.Deletar Cliente");
+                Console.WriteLine("4.Select (selecionar a tabela)");
+
+                int escolha = Convert.ToInt32(Console.ReadLine());
+
+                switch (escolha)
+
+
+                {
+
+                    case 1:
+                        incluir();
+                        break;
+
+
+                    case 2:
+                        alterar();
+                        break;
+
+
+                    case 3:
+                        deletar();
+                        break;
+
+                    case 4:
+                        select();
+                    break;
+
+                    default:
+                        Console.WriteLine("Operação inválida.");
+
+                        break;
+                }
+
+                Console.WriteLine("Deseja continuar \n");
+                op = Console.ReadLine().ToLower();
+
+            } while (op == "s");
+
+
+
+
+
+
+
+
+        }
+
+        private static void select()
+        {
+            using (var contexto = new CadastroClientesEntities())
+            {
+                // Exemplo de consulta simples para selecionar todos os usuários da agenda
+                var cadastros = contexto.CadastroClientes.ToList();
+
+                foreach (var listacadastro in cadastros)
+                {
+                    Console.WriteLine($"ID: {listacadastro.Id}, Nome: {listacadastro.NomeCliente}," +
+                        $" Endereço: {listacadastro.Endereco}, Cidade: {listacadastro.Cidade}, Telefone: {listacadastro.TelefoneContato}");
+                }
+            }
+            Console.ReadKey();
+        }
+
+        private static void deletar()
+        {
+            using (var contexto = new CadastroClientesEntities())
+            {
+                Console.WriteLine("Digite ID que deseja apagar: ");
+                var busca = contexto.CadastroClientes.Find(int.Parse(Console.ReadLine()));
+                Console.WriteLine($"Voçê esta prestes a apagar o usuario:  {busca.NomeCliente}");
+                Console.WriteLine("Tem certeza que deseja apagar, digite S para apagar ou qualquer tecla para anular ");
+                var apaga = Console.ReadLine();
+
+                if (apaga == "S")
+
+                {
+
+                    contexto.CadastroClientes.Remove(busca);
+                    contexto.SaveChanges();
+                    Console.WriteLine("Usuario excluido com sucesso");
+                }
+
+                else
+                {
+                    Console.WriteLine("Operação anulada");
+
+
+                }
+
+            }
+        }
+
+        private static void alterar()
+        {
+            using (var contexto = new CadastroClientesEntities())
+            {
+
+                Console.WriteLine("Digite o ID que deseja alterar: ");
+                var busca = contexto.CadastroClientes.Find(int.Parse(Console.ReadLine()));
+                Console.WriteLine(busca.NomeCliente);
+                Console.WriteLine(busca.TelefoneContato);
+                Console.WriteLine("Entre com a corrreção do nome: ");
+                busca.NomeCliente = Console.ReadLine();
+                Console.WriteLine("Entre com a correção do celular: ");
+                busca.TelefoneContato = Console.ReadLine();
+
+                contexto.SaveChanges();
+                Console.WriteLine("Usuario alterado com sucesso!!");
+
+
+
+
+            }
+
+        }
+
+        private static void incluir()
+        {
+            string vNome, vEndereco , vCidade ,vTelefone;
+
+            Console.WriteLine("Digite seu nome: ");
+            vNome = Console.ReadLine();
+            Console.WriteLine("Digite seu Endereço: ");
+            vEndereco = Console.ReadLine();
+            Console.WriteLine("Digite sua Cidade:");
+            vCidade = Console.ReadLine();
+            Console.WriteLine("Digite seu telefone:");
+            vTelefone = Console.ReadLine();
+
+            using (var contexto = new CadastroClientesEntities())
+            {
+                contexto.CadastroClientes.Add(new CadastroClientes()
+                {
+
+                    NomeCliente = vNome,
+                    Endereco = vEndereco,
+                    Cidade = vCidade,
+                    TelefoneContato = vTelefone
+                });
+                contexto.SaveChanges();
+                Console.WriteLine("Usuario adicionado ");
+
+
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ repos/EFtemp46 1/EFtemp46/EFtemp46/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFtemp46
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+            string op;
+
+            do
+            {
+                Console.WriteLine("Seja Bem Vindos \n");
+                Console.WriteLine("Escolha uma Opção \n");
+                Console.WriteLine("1.Incluir");
+                Console.WriteLine("2.Alterar");
+                Console.WriteLine("3.Deletar");
+                Console.WriteLine("4.Select");
+
+                int escolha = Convert.ToInt32(Console.ReadLine());
+
+                switch (escolha)
+
+
+                {
+
+                    case 1:
+                        incluir();
+                        break;
+
+
+                    case 2:
+                        alterar();
+                        break;
+
+
+                    case 3:
+                        deletar();
+                        break;
+
+                    //case 4:
+                        //lista4.menu04();
+                        //break;
+
+                    default:
+                        Console.WriteLine("Operação inválida.");
+
+                        break;
+                }
+
+                Console.WriteLine("Deseja continuar \n");
+                op = Console.ReadLine().ToLower();
+
+            } while (op == "s");
+
+
+        }
+
+        private static void deletar()
+        {
+
+            using (var contexto = new BDtemp46Entities())
+            {
+                Console.WriteLine("Digite ID que deseja apagar: ");
+                var busca = contexto.Agenda.Find(int.Parse(Console.ReadLine()));
+                Console.WriteLine($"Voçê esta prestes a apagar o usuario:  {busca.Nome}");
+                Console.WriteLine("Tem certeza que deseja apagar, digite S para apagar ou qualquer tecla para anular ");
+                var apaga = Console.ReadLine();
+
+                if (apaga == "S")
+
+                {
+
+                    contexto.Agenda.Remove(busca);
+                    contexto.SaveChanges();
+                    Console.WriteLine("Usuario excluido com sucesso");
+                }
+
+                else
+                {
+                    Console.WriteLine("Operação anulada");
+
+
+                }
+
+            }
+
+
+        }
+
+        private static void alterar()
+        {
+            using (var contexto = new BDtemp46Entities())
+            {
+
+                Console.WriteLine("Digite o ID que deseja alterar: ");
+                var busca = contexto.Agenda.Find(int.Parse(Console.ReadLine()));
+                Console.WriteLine(busca.Nome);
+                Console.WriteLine(busca.Celular);
+                Console.WriteLine("Entre com a corrreção do nome: ");
+                busca.Nome = Console.ReadLine();
+                Console.WriteLine("Entre com a correção do celular: ");
+                busca.Celular = Console.ReadLine();
+
+                contexto.SaveChanges();
+                Console.WriteLine("Usuario alterado com sucesso!!");
+
+
+
+
+            }
+
+
+
+
+
+        }
+
+        private static void incluir()
+        {
+            string vNome, vCelular;
+
+            Console.WriteLine("Digite se nome : ");
+            vNome = Console.ReadLine();
+            Console.WriteLine("Digite sem Celular ");
+            vCelular = Console.ReadLine();
+
+
+
+            using (var contexto = new BDtemp46Entities())
+            {
+                contexto.Agenda.Add(new Agenda()
+                {
+
+                    Nome = vNome,
+                    Celular = vCelular,
+                });
+                contexto.SaveChanges();
+                Console.WriteLine("Usuario adicionado ");
+
+
+            }
+
+            Console.ReadKey();
+
+
+        }
+
+    }
+}
